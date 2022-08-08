5b70ceb490aca70234d6ef0db30be053789c2387
fbaravalle <fabio.baravalle@placemake.io>
2022-08-08 21:24:38 +0100

    still not working

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_server(false)
+        .include_file("mod.rs")
         .compile(
             &[
                 "proto/messages/bar.proto",
                 "proto/messages/bar.proto",
                 "proto/services/first-service.proto",
-                "proto/services/second-service.proto"
+                "proto/services/second-service.proto",
             ],
             &["proto/"],
         )?;
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-pub mod rpc {
-    tonic::include_proto!("common");
-    tonic::include_proto!("service");
-}
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+pub mod apis {
+    tonic::include_proto!("mod");
 }
